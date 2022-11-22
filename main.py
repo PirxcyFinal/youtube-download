7f723f844defc520764901d3e612bdf7cc62cee8
Mercury <70515936+ilovekids2@users.noreply.github.com>
2022-11-22 16:29:29 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import ffmpeg
+import pytube
+import json
+
+with open("yt.json") as f:
+    config = json.load(f)
+
+video = pytube.YouTube(f'https://www.youtube.com/watch?v={config["Youtube_id"]}')
+Streams = video.streams
+highresvid = Streams.filter(res = f"{config['Guality']}", file_extension = 'mp4').first()  
+
+highresvid.download(filename=f"{config['video_name']}_video.mp4")
+stream = sorted([stream for stream in video.streams if stream.mime_type.startswith("audio")], key=lambda stream: int(stream.abr[:-4]), reverse=True)[0]
+stream.download(filename=f"{config['video_name']}_audio.mp3")
+video = ffmpeg.input(f"{config['video_name']}_video.mp4").video
+audio = ffmpeg.input(f"{config['video_name']}_audio.mp3").audio
+ffmpeg.output(
+    video,
+    audio,
+    f"{config['video_name']}.mp4",
+    vcodec='copy',
+    acodec='copy',
+).run()
+os.remove(f"{config['video_name']}_video.mp4")
+os.remove(f"{config['video_name']}_audio.mp3")
